586db3ea4db0b6fbadf771343e2afbc564875e95
Olivier Cartier <me@cestoliv.com>
2022-09-08 19:32:37 +0200

    test: ⚡️ adding more tests

--- tests/parsing_test.sh
+++ tests/parsing_test.sh
@@ -12,6 +12,14 @@ HAS_ERROR=0
 test_map () {
 	echo -en "\tTesting map ${BLUE}$1${END} "
 	OUTPUT=$(./cube3d $1)
+
+	valgrind --leak-check=full --show-leak-kinds=all --errors-for-leak-kinds=all --error-exitcode=113 ./cube3d $1 >/dev/null 2>&1
+	if [ $? -eq 113 ]; then
+		echo -en "${YELLOW}Leaks${END} "
+		HAS_ERROR=1
+	fi
+
+	#echo $VALGRIND_OUTPUT
 	if [ "$OUTPUT" != "$2" ]; then
 		HAS_ERROR=1
 		echo -en "${RED}KO${END}"
@@ -22,9 +30,17 @@ test_map () {
 	fi
 }
 
+test_map "tests/maps/m0.ok.cub" "Error. (Unable to open the map file)"
 test_map "tests/maps/m1.ok.cub" "MAP OK."
 test_map "tests/maps/m2.ok.cub" "MAP OK."
-test_map "tests/maps/m3.ko.cub" "Error. (The map does not contain all the texture information.)"
+test_map "tests/maps/m3.ko.cub" "Error. (The map does not contain all the textures and colors information)"
+test_map "tests/maps/m4.ko.cub" "Error. (The map content contains invalid characters)"
+test_map "tests/maps/m5.ko.cub" "Error. (The map content contains empty lines)"
+test_map "tests/maps/m6.ko.cub" "Error. (The map content contains invalid characters)"
+test_map "tests/maps/m7.ko.cub" "Error. (The map content contains lines with only spaces)"
+test_map "tests/maps/m8.ko.cub" "Error. (You can't set a texture more than once)"
+test_map "tests/maps/m9.ko.cub" "Error. (Unable to open texture file)"
+test_map "tests/maps/m10.ok.cub" "MAP OK."
 
 if [ $HAS_ERROR -eq 0 ]; then
 	echo -e "${GREEN}All tests passed!${END}"
