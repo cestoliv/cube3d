98c8893ad3767ac25b80bffad8638f483fb7baa7
Olivier Cartier <me@cestoliv.com>
2022-09-09 18:44:36 +0200

    test: ✨ improve newline check

--- tests/parsing_test.sh
+++ tests/parsing_test.sh
@@ -11,7 +11,8 @@ HAS_ERROR=0
 
 test_map () {
 	echo -en "\tTesting map: ${BLUE}$1${END} "
-	OUTPUT=$(./cube3d $2)
+	OUTPUT_MD5=$(./cube3d $2 | md5sum)
+	WANTED_MD5=$(echo -e $3 | md5sum)
 
 	valgrind --leak-check=full --show-leak-kinds=all --errors-for-leak-kinds=all --error-exitcode=113 ./cube3d $2 >/dev/null 2>&1
 	if [ $? -eq 113 ]; then
@@ -19,9 +20,9 @@ test_map () {
 		HAS_ERROR=1
 	fi
 
-	#echo $VALGRIND_OUTPUT
-	if [ "$OUTPUT" != "$3" ]; then
+	if [ "$OUTPUT_MD5" != "$WANTED_MD5" ]; then
 		HAS_ERROR=1
+		OUTPUT=$(./cube3d $2)
 		echo -en "${RED}KO${END}"
 		echo -en " ${BOLD}Expected:${END} $3"
 		echo -e " ${BOLD}Got:${END} $OUTPUT"
@@ -30,21 +31,22 @@ test_map () {
 	fi
 }
 
-test_map "Non-existent map file" "tests/maps/m0.ok.cub" "Error. (Unable to open the map file)"
+test_map "Non-existent map file" "tests/maps/m0.ok.cub" "Error\n(Unable to open the map file)"
 test_map "Correct map" "tests/maps/m1.ok.cub" "MAP OK."
 test_map "Correct map with spaces and newline" "tests/maps/m2.ok.cub" "MAP OK."
-test_map "Map without ceiling color" "tests/maps/m3.ko.cub" "Error. (The map does not contain all the textures and colors information)"
-test_map "Map content with invalid characters" "tests/maps/m4.ko.cub" "Error. (The map content contains invalid characters)"
-test_map "Map content with empty lines" "tests/maps/m5.ko.cub" "Error. (The map content contains empty lines)"
-test_map "Map content with whitespaces only lines" "tests/maps/m6.ko.cub" "Error. (The map content contains invalid characters)"
-test_map "Map content with spaces only lines" "tests/maps/m7.ko.cub" "Error. (The map content contains lines with only spaces)"
-test_map "Map with two north texture" "tests/maps/m8.ko.cub" "Error. (You can't set a texture more than once)"
-test_map "Map with a texture file that cannot be opened" "tests/maps/m9.ko.cub" "Error. (Unable to open texture file)"
+test_map "Map without ceiling color" "tests/maps/m3.ko.cub" "Error\n(The map does not contain all the textures and colors information)"
+test_map "Map content with invalid characters" "tests/maps/m4.ko.cub" "Error\n(The map content contains invalid characters)"
+test_map "Map content with empty lines" "tests/maps/m5.ko.cub" "Error\n(The map content contains empty lines)"
+test_map "Map content with whitespaces only lines" "tests/maps/m6.ko.cub" "Error\n(The map content contains invalid characters)"
+test_map "Map content with spaces only lines" "tests/maps/m7.ko.cub" "Error\n(The map content contains lines with only spaces)"
+test_map "Map with two north texture" "tests/maps/m8.ko.cub" "Error\n(You can't set a texture more than once)"
+test_map "Map with a texture file that cannot be opened" "tests/maps/m9.ko.cub" "Error\n(Unable to open texture file)"
 test_map "Correct map with spaces after and inside texture" "tests/maps/m10.ok.cub" "MAP OK."
-test_map "RGB color value over 255" "tests/maps/m11.ko.cub" "Error. (Color value must be between 0 and 255)"
-test_map "4-values color codes" "tests/maps/m12.ko.cub" "Error. (Invalid RGB color format)"
-test_map "RGB color with unauthorized characters" "tests/maps/m13.ko.cub" "Error. (Invalid RGB color format)"
-test_map "Map with two ceiling color" "tests/maps/m14.ko.cub" "Error. (You can't set a color more than once)"
+test_map "RGB color value over 255" "tests/maps/m11.ko.cub" "Error\n(Color value must be between 0 and 255)"
+test_map "4-values color codes" "tests/maps/m12.ko.cub" "Error\n(Invalid RGB color format)"
+test_map "RGB color with unauthorized characters" "tests/maps/m13.ko.cub" "Error\n(Invalid RGB color format)"
+test_map "Map with two ceiling color" "tests/maps/m14.ko.cub" "Error\n(You can't set a color more than once)"
+test_map "Map with multiple player positions" "tests/maps/m15.ko.cub" "Error\n(Map contains multiple positions for the player)"
 
 if [ $HAS_ERROR -eq 0 ]; then
 	echo -e "${GREEN}All tests passed!${END}"
